53baf90280fe3b688ae2deb174aa37e5a19a1c6e
Elia <mail@eliareutlinger.ch>
2019-08-07 12:02:49 +0200

    Update lStore.js

--- src/plugins/lStore.js
+++ src/plugins/lStore.js
@@ -1,31 +1,38 @@
 
 const lStore = {
 
+    name: '_app-lStore',
     stores: false,
 
     hasStore: (name) => {
-        if (!lStore.stores) return false
+        if (!lStore.stores) {
+            lStore.stores = JSON.parse(localStorage.getItem(lStore.name+'.stores'))
+            if (!lStore.stores) {
+                localStorage.setItem(lStore.name + '.stores', JSON.stringify([]))
+                lStore.stores = []
+            }
+        }
         return lStore.stores.includes(name)
     },
 
     createStore: (name) => {
-        if (!lStore.stores) lStore.stores = []
+        localStorage.setItem(lStore.name + '.store.' + name, JSON.stringify({}))
         lStore.stores.push(name)
-        localStorage.setItem('lStore.stores', JSON.stringify(lStore.stores))
-        localStorage.setItem('lStore.store.' + name, JSON.stringify([]))
+        localStorage.setItem(lStore.name + '.stores', JSON.stringify(lStore.stores))
     },
 
     get: (sName) => {
         if (!lStore.hasStore(sName)) lStore.createStore(sName)
-        return JSON.parse(localStorage.getItem('lStore.store.' + sName))
+        return JSON.parse(localStorage.getItem(lStore.name + '.store.' + sName))
     },
 
     set: (sName, data) => {
         if (!lStore.hasStore(sName)) lStore.createStore(sName)
-        localStorage.setItem('lStore.store.' + sName, JSON.stringify(data));
+        localStorage.setItem(lStore.name + '.store.' + sName, JSON.stringify(data));
     },
 
     clear: () => {
+        lStore.stores = false
         localStorage.clear()
     }
 
