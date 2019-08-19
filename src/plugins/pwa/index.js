7f078a28376d151f52b1d79d285b54a63f41679f
Elia <mail@eliareutlinger.ch>
2019-08-19 15:16:05 +0200

    hmmm

--- src/plugins/pwa/index.js
+++ src/plugins/pwa/index.js
@@ -1,5 +1,28 @@
 /* eslint-disable no-console */
 
+// This is the "Offline page" service worker
+
+// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+
+// Check compatibility for the browser we're running this in
+
+
+if ('serviceWorker' in navigator) {
+
+    if (navigator.serviceWorker.controller) {
+        console.log('[PWA Builder] active service worker found, no need to register')
+    } else {
+        // Register the service worker
+        navigator.serviceWorker.register('./pwabuilder-sw.js', {
+            scope: './'
+        }).then(function (reg) {
+            console.log('[PWA Builder] Service worker has been registered for scope: ' + reg.scope)
+        })
+    }
+}
+
+
+/*
 import { register } from 'register-service-worker'
 
 import NProgress from '@/plugins/nprogress'
@@ -31,5 +54,5 @@ if (process.env.NODE_ENV === 'production')
             console.error('Error during service worker registration:', error)
         }
     })
-
+*/
 /* eslint-enable no-console */
