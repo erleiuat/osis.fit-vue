07dee4af494118b27a1926ec1928299a498e7da0
Elia <mail@eliareutlinger.ch>
2019-09-01 13:03:18 +0200

    Format

---
+++ src/views/Main.vue
@@ -0,0 +1,30 @@
+
+<template>
+
+    <v-container class="fill-height" flex-column>
+
+        <v-row align="center" justify="center">
+            <v-col v-for="n in 3" :key="n">
+                <v-card class="pa-2" outlined tile>
+                    1. One of three columns
+                </v-card>
+            </v-col>
+        </v-row>
+
+        <v-row align="start" justify="start">
+            <v-col v-for="n in 3" :key="n">
+                <v-card class="pa-2" outlined tile>
+                    2. One of three columns
+                </v-card>
+            </v-col>
+        </v-row>
+
+    </v-container>
+
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
