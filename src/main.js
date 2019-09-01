1208799664e292d712d71470c5343e232b6f2d25
Elia <mail@eliareutlinger.ch>
2019-09-01 12:11:16 +0200

    refactor

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
-import vuetify from './plugins/vuetify';
+import router from '@/plugins/router/'
+import store from '@/plugins/vuex/'
+import vuetify from '@/plugins/vuetify/'
+import '@/plugins/pwa/'
 
 Vue.config.productionTip = false
 
