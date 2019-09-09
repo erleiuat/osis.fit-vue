24414c7c7288707656ebf9512ff33fbadd835cb8
Elia <mail@eliareutlinger.ch>
2019-09-09 16:58:27 +0200

    Update index.vue

--- src/views/premium/index.vue
+++ src/views/premium/index.vue
@@ -8,29 +8,25 @@
                 </div>
             </v-col>
             <v-col cols="12" sm="8">
-                Hole dir noch heute dein Premium-Abonnement von Osis.fit um zahlreiche Funktionen freizuschalten.
+                {{ $t('text') }}
             </v-col>
+        </v-row>
+
+        <v-row dense justify="center">
+            <v-expansion-panels>
+                <v-expansion-panel v-for="(f, key) in features" :key="key">
+                    <v-expansion-panel-header class="subheader">
+                        {{ f }}
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content eager>
+                        <v-divider />
+                        {{ f }}
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
         </v-row>
 
         <v-row justify="center">
-            <v-col cols="12">
-                <v-divider />
-            </v-col>
-        </v-row>
-
-        <v-row dense justify="center" v-for="(f, key) in features" :key="key">
-            <v-col cols="10">
-                {{ f }}
-            </v-col>
-            <v-col cols="2" class="text-center">
-                <v-icon large color="success">check</v-icon>
-            </v-col>
-        </v-row>
-
-        <v-row justify="center">
-            <v-col cols="12">
-                <v-divider />
-            </v-col>
             <v-col cols="12" sm="8">
                 <v-btn color="amber" dark depressed block :to="{name: 'settings.premium'}">
                     <strong>{{ $t('getPremium') }}</strong>
@@ -67,22 +63,26 @@ export default {
         messages: {
             en: {
                 title: 'Premium',
+                text: 'Hole dir noch heute dein Premium-Abonnement von Osis.fit um zahlreiche Funktionen freizuschalten.',
                 getPremium: 'Get Premium',
                 features: {
-                    f1: 'Esswaren mit Bildern erstellen'
+                    f1: 'Esswaren mit Bildern',
+                    f2: 'Produkt-Datenbank',
+                    f3: 'Barcode-Scanner',
+                    f4: 'Aktivitäten aufzeichen',
+                    f5: 'Übungen & Trainingspläne',
+                    f6: 'Profilbild einstellen'
                 }
             },
             de: {
                 title: 'Premium',
+                text: 'Hole dir noch heute dein Premium-Abonnement von Osis.fit um zahlreiche Funktionen freizuschalten.',
                 getPremium: 'Premium erhalten',
                 features: {
                     f1: 'Esswaren mit Bildern erstellen',
                     f2: 'Datenbank mit Produkten durchsuchen',
                     f3: 'Aktivitäten/Kalorienverbrauch aufzeichen',
-                    f4: 'Trainingspläne erstellen & druchsuchen',
-                    f5: 'Fitness-Tracker verknüpfen (bald verfügbar)',
-                    f6: 'Werbung entfernen',
-                    f7: 'Detailierten Tagesbedarf berechnen'
+                    f4: 'Trainingspläne erstellen & druchsuchen'
                 }
             }
         }
