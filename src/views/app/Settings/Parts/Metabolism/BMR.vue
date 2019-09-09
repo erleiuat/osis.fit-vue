89fcf3e19b9a3740b0895aa9bb706095fc6d0128
Elia <mail@eliareutlinger.ch>
2019-09-09 15:39:51 +0200

    Update BMR.vue

--- src/views/app/Settings/Parts/Metabolism/BMR.vue
+++ src/views/app/Settings/Parts/Metabolism/BMR.vue
@@ -22,21 +22,21 @@
             <v-select :label="$t('gender')" v-model="fd.gender" :items="genders" :rules="rule.require" filled />
         </v-col>
 
-        <v-col cols="12" md="5">
-            <v-text-field v-model="fd.birthdate" :label="$t('birthdate')" type="date" append-icon="event" filled />
-        </v-col>
-
         <!-- TODO
-        <v-col cols="12" md="1">
-            <v-text-field :label="$t('day')" type="number" filled />
-        </v-col>
-        <v-col cols="12" md="1">
-            <v-text-field :label="$t('month')" type="number" filled />
-        </v-col>
-        <v-col cols="12" md="3">
-            <v-text-field :label="$t('year')" type="number" filled />
+        <v-col cols="12" md="5">
+            <v-text-field v-model="formBirthdate" :label="$t('birthdate')" type="text" append-icon="event" filled />
         </v-col>
         -->
+
+        <v-col cols="12" md="1">
+            <v-text-field v-model="fbdDay" :rules="rule.require" placeholder="DD" type="number" minlength="2" maxlength="2" min="1" max="31" filled />
+        </v-col>
+        <v-col cols="12" md="1">
+            <v-text-field v-model="fbdMonth" :rules="rule.require" placeholder="MM" type="number" minlength="2" maxlength="2" min="1" max="12" filled />
+        </v-col>
+        <v-col cols="12" md="3">
+            <v-text-field v-model="fbdYear" :rules="rule.require" placeholder="YYYY" type="number" minlength="4" maxlength="4" min="1990" :max="curYear" filled />
+        </v-col>
 
         <v-col cols="12" class="text-center">
             {{ bmr }}
@@ -60,6 +60,67 @@ export default {
     ],
 
     computed: {
+
+        curYear(){
+            var nDate = this.$store.getters['today'].date.split('-')
+            return nDate[0]
+        },
+
+        fbdDay: {
+            get () {
+                if (this.fd.birthdate) {
+                    var nDate = this.fd.birthdate.split('-')
+                    return nDate[2]
+                }
+                return 0
+            },
+            set (val) {
+                val = parseInt(val)
+                if (val <= 0) val = '00'
+                else if (val <= 9) val = '0' + val
+                else if (val > 31) val = 31
+                var nDate = this.fd.birthdate.split('-')
+                this.fd.birthdate = nDate[0] + '-' + nDate[1] + '-' + val
+            }
+        },
+
+        fbdMonth: {
+            get () {
+                if (this.fd.birthdate) {
+                    var nDate = this.fd.birthdate.split('-')
+                    return nDate[1]
+                }
+                return 0
+            },
+            set (val) {
+                val = parseInt(val)
+                if (val <= 0) val = '00'
+                else if (val <= 9) val = '0' + val
+                else if (val > 12) val = 12
+                var nDate = this.fd.birthdate.split('-')
+                this.fd.birthdate = nDate[0] + '-' + val + '-' + nDate[2]
+            }
+        },
+
+        fbdYear: {
+            get () {
+                if (this.fd.birthdate) {
+                    var nDate = this.fd.birthdate.split('-')
+                    return nDate[0]
+                }
+                return 0
+            },
+            set (val) {
+                val = parseInt(val)
+                if (val <= 0) val = '0000'
+                else if (val <= 9) val = '000' + val
+                else if (val <= 99) val = '00' + val
+                else if (val <= 999) val = '0' + val
+                else if (val > this.curYear) val = this.curYear
+                var nDate = this.fd.birthdate.split('-')
+                this.fd.birthdate = val + '-' + nDate[1] + '-' + nDate[2]
+            }
+        },
 
         bmr () {
             var rtn = this.$t('bmr') + ': '
