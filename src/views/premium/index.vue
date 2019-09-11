1764b9ad67a17c00b92d279e3409f88454426e28
Elia <mail@eliareutlinger.ch>
2019-09-11 12:43:40 +0200

    Update index.vue

--- src/views/premium/index.vue
+++ src/views/premium/index.vue
@@ -13,18 +13,30 @@
         </v-row>
 
         <v-row dense justify="center">
-            <v-expansion-panels>
-                <v-expansion-panel v-for="(f, key) in features" :key="key">
-                    <v-expansion-panel-header class="subheader">
-                        {{ f }}
-                    </v-expansion-panel-header>
-                    <v-expansion-panel-content eager>
-                        <v-divider />
-                        {{ f }}
-                    </v-expansion-panel-content>
-                </v-expansion-panel>
-            </v-expansion-panels>
+            <v-col cols="12" sm="8">
+                <v-expansion-panels>
+                    <v-expansion-panel v-for="(f, key) in features" :key="key">
+                        <v-expansion-panel-header class="subheader">
+                            {{ f.title }}
+                        </v-expansion-panel-header>
+                        <v-expansion-panel-content eager>
+                            {{ f.text }}
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                </v-expansion-panels>
+            </v-col>
         </v-row>
+
+        <!-- TODO
+        <v-row justify="center">
+            <v-col cols="12" sm="8">
+                {{ $t('priceText') }}
+            </v-col>
+            <v-col cols="12" sm="8">
+                10
+            </v-col>
+        </v-row>
+        -->
 
         <v-row justify="center">
             <v-col cols="12" sm="8">
@@ -44,13 +56,13 @@ export default {
     computed: {
         features () {
             return [
-                this.$t('features.f1'),
-                this.$t('features.f2'),
-                this.$t('features.f3'),
-                this.$t('features.f4'),
-                this.$t('features.f5'),
-                this.$t('features.f6'),
-                this.$t('features.f7')
+                { title: this.$t('features.f1.title'), text: this.$t('features.f1.text') },
+                { title: this.$t('features.f2.title'), text: this.$t('features.f2.text') },
+                { title: this.$t('features.f3.title'), text: this.$t('features.f3.text') },
+                { title: this.$t('features.f4.title'), text: this.$t('features.f4.text') },
+                { title: this.$t('features.f5.title'), text: this.$t('features.f5.text') },
+                { title: this.$t('features.f6.title'), text: this.$t('features.f6.text') },
+                { title: this.$t('features.f7.title'), text: this.$t('features.f7.text') }
             ]
         }
     },
@@ -63,27 +75,72 @@ export default {
         messages: {
             en: {
                 title: 'Premium',
-                text: 'Hole dir noch heute dein Premium-Abonnement von Osis.fit um zahlreiche Funktionen freizuschalten.',
-                getPremium: 'Get Premium',
+                text: 'Get your premium Osis.fit subscription today to unlock many additional features.',
+                getPremium: 'Get Premium!',
                 features: {
-                    f1: 'Esswaren mit Bildern',
-                    f2: 'Produkt-Datenbank',
-                    f3: 'Barcode-Scanner',
-                    f4: 'Aktivitäten aufzeichen',
-                    f5: 'Übungen & Trainingspläne',
-                    f6: 'Profilbild einstellen',
-                    f7: 'Priorisierter Support'
+                    f1: {
+                        title: 'Edibles with pictures',
+                        text: 'Create your own templates and add an image to find the entry faster.'
+                    },
+                    f2: {
+                        title: 'Food database',
+                        text: 'Search a huge database of products to get the nutritional values faster.'
+                    },
+                    f3: {
+                        title: 'Barcode-Scanner',
+                        text: 'Scan the barcode of a product to preserve its nutritional value.'
+                    },
+                    f4: {
+                        title: 'Track activities',
+                        text: 'Record additional activities to accurately calculate your calorie balance.'
+                    },
+                    f5: {
+                        title: 'Exercises & Training Plans',
+                        text: 'Browse or create fitness exercises to add to a training plan, or explore new training plans with our templates.'
+                    },
+                    f6: {
+                        title: 'Add profile picture',
+                        text: 'Add a profile picture to your account to further personalize the app and make it easier to be recognized by (upcoming) community features.'
+                    },
+                    f7: {
+                        title: 'Priority Support',
+                        text: 'Problems or questions about the app? Support requests from premium users are prioritized and processed faster.'
+                    }
                 }
             },
             de: {
                 title: 'Premium',
-                text: 'Hole dir noch heute dein Premium-Abonnement von Osis.fit um zahlreiche Funktionen freizuschalten.',
-                getPremium: 'Premium erhalten',
+                text: 'Hole dir noch heute dein Premium-Abonnement von Osis.fit um zahlreiche zusätzliche Funktionen freizuschalten.',
+                getPremium: 'Premium holen!',
                 features: {
-                    f1: 'Esswaren mit Bildern erstellen',
-                    f2: 'Datenbank mit Produkten durchsuchen',
-                    f3: 'Aktivitäten/Kalorienverbrauch aufzeichen',
-                    f4: 'Trainingspläne erstellen & druchsuchen'
+                    f1: {
+                        title: 'Esswaren mit Bildern',
+                        text: 'Erstelle eigene Vorlagen und füge ein Bild hinzu, um den Eintrag später schneller zu finden.'
+                    },
+                    f2: {
+                        title: 'Lebensmittel-Datenbank',
+                        text: 'Durchsuche eine riesige Datenbank von Produkten um schneller die Nährwerte zu erhalten.'
+                    },
+                    f3: {
+                        title: 'Barcode-Scanner',
+                        text: 'Scanne den Barcode eines Produkts um dessen Nährwerte zu erhalten.'
+                    },
+                    f4: {
+                        title: 'Aktivitäten aufzeichen',
+                        text: 'Zeiche zusätzliche Aktivitäten auf, um deine Kalorienbilanz genau zu berechnen.'
+                    },
+                    f5: {
+                        title: 'Übungen & Trainingspläne',
+                        text: 'Durchsuche oder erstelle Fitness-Übungen um diese einem Trainingsplan hinzuzufügen, oder finde neue Trainingspläne durch unsere Vorlagen.'
+                    },
+                    f6: {
+                        title: 'Profilbild einstellen',
+                        text: 'Füge ein Profilbild zu deinem Konto hinzu, um die App weiter zu personalisieren und bei (kommenden) Community-Funktionen besser erkannt zu werden.'
+                    },
+                    f7: {
+                        title: 'Priorisierter Support',
+                        text: 'Probleme oder Fragen zur App? Support-Anfragen von Premium-Nutzern werden priorisiert und schneller bearbeitet.'
+                    }
                 }
             }
         }
